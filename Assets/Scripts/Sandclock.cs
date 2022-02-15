c443d22410837615176f4f31fb01b704f3c3fc78
Cheune13 <80524613+Chreune13@users.noreply.github.com>
2022-02-15 14:33:01 +0100

    rescale map

---
+++ Assets/Scripts/Sandclock.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Sandclock : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody hourglass;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hourglass.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    public void OnPlayerGrabbed(SelectEnterEventArgs args)
+    {
+        hourglass.isKinematic = false;
+    }
+}
